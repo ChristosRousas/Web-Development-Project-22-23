dfcbad2bde09e04a96b95167d86fe0a066a9812a
Blynx <62656181+Blynx02@users.noreply.github.com>
2023-01-10 00:21:13 +0200

    Add files via upload
    index.js που αντιμετωπίζει τα post αιτήματα του ερωτήματος 3

--- Ergasia2/index.js
+++ Ergasia2/index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 8080
-
+const { v4: uuidv4 } = require('uuid');
 app.listen(port)
-
+users=[{
+    "username" : "Username1",
+    "password" : "Password1",
+    "cart" : []
+},{
+    "username" : "Username2",
+    "password" : "Password2",
+    "cart" : []
+}]
 /* 
     Serve static content from directory "public",
     it will be accessible under path /, 
@@ -29,3 +37,96 @@ app.get('/', function(req, res){
         console.log(err)
     })
 })
+
+app.post('/login', (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    let found = false;
+    var i=0;
+    while(i<users.length && !found){
+        if(username===users[i].username){
+            if(password === users[i].password){
+                found = true;
+            }
+            else{
+                i++;
+            }
+        }else{
+            i++;
+        }
+    }
+    if(found){
+        const sessionId = uuidv4();
+        console.log("FOUND")
+        res.json({"sessionId" : sessionId })
+        users[i].sessionId = sessionId;
+    }
+    else{
+        console.log("NOT FOUND")
+        res.json(400)
+    }
+})
+
+app.post('/cart', (req,res)=>{
+    var costumer = FindCostumer(req)
+    var i = costumer.i
+    let found = costumer.found
+    if(found){
+        var p=0;
+        found = false
+        while(p<users[i].cart.length && !found){
+            if(users[i].cart[p].id == req.body.data.id){
+                found = true
+                users[i].cart[p].quantity++
+            }else{
+                p++
+            }
+        }
+        if(!found){
+            req.body.data.quantity =1
+            users[i].cart.push(req.body.data)
+        }
+        console.log("FOUND")
+        res.json(200)
+    }
+    else{
+        console.log("NOT FOUND")
+        res.json(400)
+    }
+
+})
+
+app.post('/cart_size',(req,res) => {
+    var costumer = FindCostumer(req)
+    var i = costumer.i
+    let found = costumer.found
+    if(found){
+        var sum = 0;
+        for(p=0;p<users[i].cart.length;p++){
+            sum+= users[i].cart[p].quantity
+        }
+    res.json({"size" : sum})
+}
+else{
+    res.json(400)
+}
+})
+
+function FindCostumer(req){
+    let found = false;
+    let i =0;
+    while(i<users.length&& !found){
+        if(req.body.username===users[i].username){
+            if(req.body.sessionId.sessionId===users[i].sessionId){
+                found=true;
+            }
+            else{
+                found=false
+            }
+        }
+        else{
+            i++
+        }
+    }
+    return {found,i};
+}
